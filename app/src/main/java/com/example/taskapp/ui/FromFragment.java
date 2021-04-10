bc1092af761743f7dd0511f6966ab81bfa9d6b50
samat75 <samatorozov2005@gmail.com>
2021-04-10 11:51:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/ui/FromFragment.java
@@ -0,0 +1,38 @@
+package com.example.taskapp.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.taskapp.R;
+
+public class FromFragment extends Fragment {
+    private EditText editText;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_from, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        editText = view.findViewById(R.id.editText);
+        view.findViewById(R.id.btnSave).setOnClickListener(v -> {
+            save();
+        });
+    }
+
+    private void save() {
+        String text = editText.getText().toString();
+
+    }
+}
---
+++ app/src/main/java/com/example/taskapp/ui/home/HomeFragment.java
@@ -0,0 +1,37 @@
+package com.example.taskapp.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.taskapp.R;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.fad).setOnClickListener(view1 -> {
+            openFrom();
+        });
+    }
+
+    private void openFrom() {
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+        navController.navigate(R.id.fromFragment);
+    }
+}
